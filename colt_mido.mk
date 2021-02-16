9777d0500288bc4814f71692acb1fa959432144c
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-02-16 16:03:24 +0000

    mido: get colt-ify

--- colt_mido.mk
+++ colt_mido.mk
@@ -21,21 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common ColtOS stuff.
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Official
-XTENDED_BUILD_TYPE := UnOfficial
+COLT_BUILD_TYPE := UnOfficial
 
 # Maintainer
-XTENDED_MAINTAINER := Pratyaksh_Bharadwaj
+COLT_DEVICE_MAINTAINER := Pratyaksh_Bharadwaj
 
 # GApps
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := xtended_mido
+PRODUCT_NAME := colt_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
