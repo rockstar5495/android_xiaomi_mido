47c89d3b5bddb3c7adba821a23490a51ca1b0fb6
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-02-16 14:51:52 +0000

    mido: get xtended

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -21,12 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Official
+XTENDED_BUILD_TYPE := UnOfficial
+
+# Maintainer
+XTENDED_MAINTAINER := Pratyaksh_Bharadwaj
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := xtended_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
