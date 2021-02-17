a8746cc86de1c30c6988b962565c8c685ed0aa26
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-02-17 18:32:38 +0000

    Xtended Mido: build with official tag

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Official
-XTENDED_BUILD_TYPE := UnOfficial
+XTENDED_BUILD_TYPE := Official
 
 # Maintainer
 XTENDED_MAINTAINER := 🔥Pratyaksh💥Bharadwaj🔥
