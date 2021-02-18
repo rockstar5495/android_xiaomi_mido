fc8743041a361156c686caefe04d2d908611df1c
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-02-18 04:57:02 +0000

    mido: add bootanimation flag

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -33,6 +33,9 @@ XTENDED_MAINTAINER := 🔥Pratyaksh💥Bharadwaj🔥
 # GApps
 TARGET_GAPPS_ARCH := arm64
 
+# Mido bootanimation flag
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := xtended_mido
